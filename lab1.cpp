b6c55fdf2c79c1f10826bf73deaf86a72750dd5c
lokcsit25-cpu <lokcsit25@oic.edu.np>
2026-06-08 14:32:59 +0545

    initial commit

---
+++ lab1.cpp
@@ -0,0 +1,36 @@
+/* (1) There is a vacancy for a seasonal job. Applicants are qualified to apply for the job only if they
+can lift at least 50 kg weight and are between 18 to 24 yrs of age. However, if the applicants
+have more than 4 yrs of experience in the field, they can qualify even if they can lift just 35 kg
+and are over 24 yrs of age. Write a program in C++ to input the information of applicants and
+check if they meet the required credentials for the job.
+(2) Write a program to output the nth Fibonacci number using iteration.
+(3) Write a program that prompts a student to enter the marks they obtained in Object
+Oriented Programming. Output the corresponding grade they obtained. (0-59 = F, 60-69 = D,
+70-79 = C, 80-89 = B, 90-100 = A) */
+#include<iostream>
+using namespace std;
+int main()
+{
+	float weight;
+	cout<<"Enter lifted weight of applicant: ";
+	cin>>weight;
+	int age;
+	cout<<"Enter age of applicant: ";
+	cin>>age;
+	float experience;
+	cout<<"Enter experience of applicant: ";
+	cin>>experience;
+	if(age>=18&&age<=24&&weight>=50)
+	{
+			cout<<"Applicant meet the required credentials!";
+	}
+	else if(experience >= 4 && weight>=35&& age>=18 )
+	{
+		cout<<"Applicant meet the required credentials!";
+	}
+	else
+	{
+		cout<<"Applicant does not meet the required credentials";
+	}
+return 0;	
+}
