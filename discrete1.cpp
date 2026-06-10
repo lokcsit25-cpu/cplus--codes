bb3ecc1a4b84caa7da757c0c7b22217591d06139
lokcsit25-cpu <lokcsit25@oic.edu.np>
2026-06-10 20:57:26 +0545

    Discrete lab1

--- discrete1.cpp
+++ discrete1.cpp
@@ -1,21 +1,21 @@
 // to find intersection of two sets
-#include<stdio.h>
-int main(){
-	int Uset[10]={10,11,12,13,14,15,16,17,18,19};
-	int setA[4]={11,12,13,18};
-	int setB[4]={14,16,18,19};
-	int i,j;
-	printf("Intersection of two sets: { ");
-	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			if(setA[i]==setB[j]){
-				printf("%d ",setA[i]);
-			}
-		}
-	}
-	printf("}");
+// #include<stdio.h>
+// int main(){
+// 	int Uset[10]={10,11,12,13,14,15,16,17,18,19};
+// 	int setA[4]={11,12,13,18};
+// 	int setB[4]={14,16,18,19};
+// 	int i,j;
+// 	printf("Intersection of two sets: { ");
+// 	for(i=0;i<4;i++){
+// 		for(j=0;j<4;j++){
+// 			if(setA[i]==setB[j]){
+// 				printf("%d ",setA[i]);
+// 			}
+// 		}
+// 	}
+// 	printf("}");
 	
-}
+// }
 // to find the union of two sets
 // #include <stdio.h>
 
@@ -50,4 +50,77 @@ int main(){
 //     printf("}");
 
 //     return 0;
+// }
+// to find the difference of two sets
+// #include <stdio.h>
+// using namespace std;
+// int main(){
+// 	int Uset[10]={10,11,12,13,14,15,16,17,18,19};
+//  	int setA[4]={11,12,13,18};
+//  	int setB[4]={14,16,18,19};
+//  	int i,j;
+//  	printf("Difference of two sets: { ");
+//  	for(i=0;i<4;i++){
+//  		int found = 0;
+//  		for(j=0;j<4;j++){
+//  			if(setA[i]==setB[j]){
+//  				found = 1;
+//  				break;
+//  			}
+//  		}
+//  		if(!found){
+//  			printf("%d ",setA[i]);
+//  		}
+//  	}
+//  	printf("}");
+// }
+// to find inverse of a set
+// #include <stdio.h>
+// using namespace std;
+// int main(){
+// 	int Uset[10]={10,11,12,13,14,15,16,17,18,19};
+//  	int setA[4]={11,12,13,18};
+//  	int i,j;
+//  	printf("Inverse of a set: { ");
+//  	for(i=0;i<10;i++){
+//  		int found = 0;
+//  		for(j=0;j<4;j++){
+//  			if(Uset[i]==setA[j]){
+//  				found = 1;
+//  				break;
+//  			}
+//  		}
+//  		if(!found){
+//  			printf("%d ",Uset[i]);
+//  		}
+//  	}
+//  	printf("}");
+// }
+// to find cross product of two sets
+// #include <stdio.h>
+// using namespace std;
+// int main(){
+// 	int Uset[10]={10,11,12,13,14,15,16,17,18,19};
+//  	int setA[4]={11,12,13,18};
+//  	int setB[4]={14,16,18,19};
+//  	int i,j;
+//  	printf("Cross product of two sets: { ");
+//  	for(i=0;i<4;i++){
+//  		for(j=0;j<4;j++){
+//  			printf("(%d,%d) ",setA[i],setB[j]);
+//  		}
+//  	}
+//  	printf("}");
+//  }
+// floor and ceiling function use
+// #include <stdio.h>
+// #include <math.h>
+// using namespace std;
+// int main(){
+// 	float num;
+// 	printf("Enter a number: ");
+// 	scanf("%f",&num);
+// 	printf("Floor of %.2f is %.2f\n",num,floor(num));
+// 	printf("Ceiling of %.2f is %.2f\n",num,ceil(num));
+// 	return 0;
 // }
