becc203b0c2fdb8c153488b4578ac0806c673f22
lokcsit25-cpu <lokcsit25@oic.edu.np>
2026-06-15 21:32:29 +0545

    xyz

--- lab2ques1.cpp
+++ lab2ques1.cpp
@@ -2,14 +2,44 @@
 of the number entered being integer or floating point. Print the result as output.*/
 #include<iostream>
 using namespace std;
-float product(float a, float b, float c){
-    return a*b*c;
-}
+void calculate(int a,int b, int c);
+void calculate(float a,float b, float c);
 int main(){
-    float num1,num2,num3;
-    cout<<"Enter three numbers: ";
-    cin>>num1>>num2>>num3;
-    float ans=product(num1,num2,num3);
-    cout<<"Product of three numbers: "<<ans<<endl;
+    
+    int choice;
+    cout << "-------------Menu-------------" << endl;
+    cout << "1) product of three integers "<<endl<<"2) product of three floating point numbers"<<endl;
+    cout<<"Enter your choice: ";
+    cin >> choice;
+
+    switch(choice){
+        case 1: {
+            int a,b,c;
+            cout << "Enter three integers: ";
+            cin >> a >> b >> c;
+            calculate(a,b,c);
+            break;
+        }
+        case 2: {
+            float a,b,c;
+            cout << "Enter three floating point numbers: ";
+            cin >> a >> b >> c;
+            calculate(a,b,c);
+            break;
+        }
+        default:
+            cout << "Invalid choice!" << endl;
+    }
+    return 0;
+}
+
+void calculate(int a,int b, int c){
+    int product = a * b * c;
+    cout << "Product of the three integers: " << product << endl;
+}
+
+void calculate(float a,float b, float c){
+    float product = a * b * c;
+    cout << "Product of the three floating point numbers: " << product << endl;
 }
 
